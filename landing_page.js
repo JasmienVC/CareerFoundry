6fa9df1336cb12a2829658dab86aa6914c301bb9
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-11-28 10:33:00 +0100

    gets detailed info for all courses on click event

--- landing_page.js
+++ landing_page.js
@@ -1,20 +1,34 @@
 const courses = document.querySelector('#courses');
 const url = "https://private-e05942-courses22.apiary-mock.com/courses";
-const my_object = {}
+const info = document.querySelector('#info');
+const array = []
 
 
 
 fetch(url)
   .then(response => response.json())
   .then((data) => {
+    let i = 0
       data.forEach((result) => {
-      my_object[result.title] = result.slug
-      const course = `<li class="course-item"><a href="">${result.title}</a></li>`
-      courses.insertAdjacentHTML("beforeend", course)
+      array.push(result.slug)
+      const course = `<li id=course-item-${i}><a href="">${result.title}</a></li>`
+      courses.insertAdjacentHTML("beforeend",  course)
+      i = i + 1
     })
   })
 
-courses.addEventListener("click", (event) => {
-  event.preventDefault() // <-- to prevent <form>'s native behaviour
-  console.log(event.currentTarget)
+document.addEventListener("click", (event) => {
+  event.preventDefault()
+  fetch(`${url}/${array[0]}`)
+    .then(response => response.json())
+    .then((data) => {
+      info.innerHTML = ""
+      info.insertAdjacentHTML("beforeend", `
+      <p>Description: ${data.description}</p>
+      <p>Next start date: ${data.start_dates[0]}</p>
+      <p> All start dates: ${data.start_dates}</p>
+      <p>Price in dollar: ${data.prices[0].amount}$</p>
+      <p>Price in euro: ${data.prices[1].amount}€</p>
+      `)
+    })
 })
